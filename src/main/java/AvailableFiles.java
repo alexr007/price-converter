f460070abd36672b5b5d3b2b591d8dbc4e9c8a76
alex <alexey.rykhalskiy@gmail.com>
2017-02-12 23:23:20 +0200

    process all *.csv *.txt in current folder

---
+++ src/main/java/AvailableFiles.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
+/**
+ * Created by alexr on 12.02.2017.
+ */
+public class AvailableFiles {
+    private final List<String> files = new ArrayList<>();
+
+    public List<String> get() throws IOException {
+        files.clear();
+        DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("."));
+        for (Path path : stream) {
+            if(!path.toFile().isDirectory()) {
+                String fileName = path.toFile().toString().toLowerCase();
+                if (fileName.endsWith(".txt")||fileName.endsWith(".csv")) {
+                    files.add(fileName);
+                }
+            }
+        }
+        return files;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,17 @@
-import proc.*;
-
-import java.io.*;
+import java.io.IOException;
 
 /**
  * Created by alexr on 09.02.2017.
+ *
+ * main class
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Price Corrector");
-        if (args.length<1) {
-            System.out.println("Usage corr.jar <file>");
-        } else {
-            new ConvertedFile(args[0])
-            .convert();
+        System.out.println("-= Price converter =-");
+        for (String name : new AvailableFiles().get()) {
+            System.out.println(String.format("Processing file: %s...", name));
+            new ConvertedFile(name).convert();
         }
+        System.out.println("-= Done =-");
     }
 }
